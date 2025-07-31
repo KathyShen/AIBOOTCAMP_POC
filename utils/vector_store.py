2f648057665262218b0fd579300e5631400c93c1
Kathy Shen <shenkai.kathy@gmail.com>
2025-07-31 16:43:04 +0800

    requirement add versioning

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -3,12 +3,19 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 
-def save_to_chroma(docs, persist_directory="vector_db"):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
-    splits = splitter.split_documents(docs)
+def save_to_chroma(docs, persist_directory="vector_db", embedding=None, chunk_size=500, chunk_overlap=100):
+    from langchain.text_splitter import RecursiveCharacterTextSplitter
+    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
+    # If docs are already split, skip splitting
+    if hasattr(docs, '__iter__') and all(isinstance(doc, str) for doc in docs):
+        splits = docs
+    else:
+        splits = splitter.split_documents(docs)
+    if embedding is None:
+        embedding = OpenAIEmbeddings()
     db = Chroma.from_documents(
         documents=splits,
-        embedding=OpenAIEmbeddings(),
+        embedding=embedding,
         persist_directory=persist_directory
     )
     db.persist()
